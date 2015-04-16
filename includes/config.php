9369d0275982977dbfa423aea27eed287db6cc00
jegtnes <alex@jegtnes.co.uk>
2015-04-16 20:53:25 +0100

    initial stuffs

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'twilio' => array(
+        'account_sid'  => 'AC2c31621d25f688a71accb740b11a097e',
+        'auth_token'   => '503c7b22a64664f04eac148d2669b181',
+        'phone_number' => '+441202286789',
+    ),
+);
+?>
